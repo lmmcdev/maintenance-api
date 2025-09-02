d648ac3f2b6662b163793f2421b51b4a2bb354d4
Alien Tapia <alientapia33@gmail.com>
2025-09-02 13:30:51 -0400

    feat(person): update PersonRole to use enum and adjust related DTO and model

--- src/modules/person/person.model.ts
+++ src/modules/person/person.model.ts
@@ -1,10 +1,11 @@
 // src/modules/person/person.model.ts
 import type { BaseDocument } from '../../infra/cosmos.repository';
+import { PersonRole } from '../../shared';
 
 export interface PersonModel extends BaseDocument {
   firstName: string;
   lastName: string;
   phoneNumber?: string;
   email?: string;
-  role?: 'admin' | 'user';
+  role?: PersonRole;
 }
